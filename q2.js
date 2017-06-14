b39dd2c309f567d7154edd6866e2ec1349beab5b
Becca <becca.liss@gmail.com>
2017-06-13 20:25:56 -0400

    Created an ugly React Counter component

--- q2.js
+++ q2.js
@@ -3,3 +3,38 @@
 // 2. Increments the counter by 1 when the user clicks an up arrow
 // 3. Decreases the counter by 1 when the user clicks a down arrow
 // Non-react developers may alternatively use vanilla js
+
+const React = require('react');
+const ReactDOM = require('react-dom');
+
+class Counter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: 0
+    }
+  }
+
+  // TODO: use getters/setters to automatically
+  updateValue(value) {
+    const newValue = this.state.value + value;
+    if (newValue <= 100 && newValue >= 0) {
+      console.log(`setting state to ${value}`);
+      this.setState({value: newValue});
+    } else {
+      console.log("Value is out of bounds!")
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <div onChange={this.updateValue}>{this.state.value}</div>
+        <button onClick={this.updateValue.bind(this, 1)}>up</button>
+        <button onClick={this.updateValue.bind(this, -1)}>down</button>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<Counter/>, document.getElementById('react-counter'));
